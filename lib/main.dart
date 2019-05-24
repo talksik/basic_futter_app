c6de0ce9b2c02f5c15e48415df0d1a7593cd8c14
talksik <patel.arjun50@gmail.com>
2019-05-24 09:19:05 -0700

    adding stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Test'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Do Everything Bar'),
-        ),
-        body: Card(
-            child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Food Paradise')
-        ])),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Do Everything Bar'),
       ),
-    );
+      body: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.all(10),
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _products.add('Advanced Food Tester');
+                  });                  
+                },
+                child: Text('Click Me!'),
+              )),
+          Column(
+            children: _products
+                .map((elem) => Card(
+                        child: Column(
+                      children: <Widget>[
+                        Image.asset('assets/food.jpg'),
+                        Text(elem)
+                      ],
+                    )))
+                .toList(),
+          ),
+        ],
+      ),
+    ));
   }
 }
